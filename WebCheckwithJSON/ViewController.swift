ab3558f2b3c051633fcad0ed60c514c3901dbe2b
SPENCER <taspence@gssla40042236.gsfc.nasa.gov>
2015-07-06 08:17:27 -0400

    Initial Commit

---
+++ WebCheckwithJSON/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebCheckwithJSON
+//
+//  Created by SPENCER, TODD A. (GSFC-7620) on 7/6/15.
+//  Copyright (c) 2015 Todd Spencer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
